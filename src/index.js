ad307d4ddc8b3fe254bb372f00484e6362bd482b
Yevheniia Soloviova <65976186+yevheniia-soloviova@users.noreply.github.com>
2020-11-10 22:16:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,48 +1,52 @@
-import foodCardTpl from "./templates/menuItems.hbs"
+import menuItemTpl from "./templates/menuItems.hbs"
 import dishes from "./menu.json"
 
-//necessary variables
-const dishesListRef = document.querySelector(".js-menu");
-const switchRef = document.querySelector("#theme-switch-toggle");
-const bodyRef = document.querySelector("body");
-const Theme = {
+const menuItemMarkup = createMenuItemMarkup(dishes);
+// Создаем переменную, в которую записываем результат вызова ф-и
+// которая создает разметку, и передаем в нее список блюд.
+const galleryList = document.querySelector('.js-menu');
+// console.log(galaryList);
+const ingredientsList = document.querySelector('.tag-list');
+
+galleryList.insertAdjacentHTML("beforeend", menuItemMarkup);
+// вставляем
+function createMenuItemMarkup(dishes) {
+  //   return dishes.map((dish) => menuItemTpl(dish)).join("");
+  return dishes.map(menuItemTpl).join("");
+};
+const bodyEl = document.querySelector('body');
+const themeAdjusterCheckboxEl = document.querySelector('.theme-switch__toggle');
+themeAdjusterCheckboxEl.addEventListener('change', onThemeChange);
+
+const theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
-//create HTML through HANDLEBAR plugin
-const markup = foodCardTpl(dishes)
-
-//add HTML on webpage
-dishesListRef.insertAdjacentHTML("beforeend", markup)
-
-//add listener for checkbox
-switchRef.addEventListener("change", changeTheme)
-
-//change theme function
-function changeTheme() {
-  if (bodyRef.classList.contains(Theme.LIGHT)) {
-    bodyRef.classList.remove(Theme.LIGHT)
-    bodyRef.classList.add(Theme.DARK)
-    localStorage.setItem("theme", Theme.DARK)
-  } else if (bodyRef.classList.contains(Theme.DARK)) {
-    bodyRef.classList.remove(Theme.DARK)
-    bodyRef.classList.add(Theme.LIGHT)
-    localStorage.setItem("theme", Theme.LIGHT)
-  } 
+function onThemeChange(evt) {
+  const currentTheme = evt.currentTarget.checked ? 'dark-theme' : 'light-theme';
+  localStorage.setItem('pageTheme', currentTheme);
+  const savedTheme = localStorage.getItem('pageTheme');
+  console.log(savedTheme);
+  if (savedTheme === theme.LIGHT) {
+    bodyEl.classList.remove(theme.DARK);
+    bodyEl.classList.add(theme.LIGHT);
+  } else {
+    bodyEl.classList.remove(theme.LIGHT);
+    bodyEl.classList.add(theme.DARK);
+  }
 }
 
-// Check fo visited before and put chosen theme
-populateSwitch()
-function populateSwitch() { 
-    const chosenTheme = localStorage.getItem("theme")
-  if (chosenTheme) {
-    if (chosenTheme === Theme.DARK) {
-      bodyRef.classList.add(Theme.DARK)
-      switchRef.checked = true;
-    } else if (chosenTheme === Theme.LIGHT) {
-      bodyRef.classList.add(Theme.LIGHT)
-      switchRef.checked = false;
-    }
-  } else { bodyRef.classList.add(Theme.LIGHT)}
+onLocalStorageChange();
+function onLocalStorageChange() {
+if (
+    localStorage.getItem('pageTheme') === null ||
+    localStorage.getItem('pageTheme') === 'light-theme'
+  ) {
+    bodyEl.classList.add(theme.LIGHT);
+    // themeAdjusterCheckboxEl.checked = false;
+  } else {
+    bodyEl.classList.add(theme.DARK);
+    themeAdjusterCheckboxEl.checked = true;
   }
+}
